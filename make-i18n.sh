f2db1c7607a729d8a5c17c3b277dda95f812d1bd
Krzysztof Płocharz <krzysztof@plocharz.info>
2015-01-03 19:15:18 +0100

    update i18n scripts with default values

--- make-i18n.sh
+++ make-i18n.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-rm locale/*/LC_MESSAGES/*.mo
+L=${1-pl}
+rm locale/$L/LC_MESSAGES/*.mo
 rm -R build/doctrees
 rm -R build/html
-sphinx-intl -l $1 -d source/locale build
-make -e SPHINXOPTS="-E -a -v -D language='$1'" html
-
+sphinx-intl -l $L -d source/locale build
+make -e SPHINXOPTS="-E -a -v -D language='$L'" html
--- update-i18n.sh
+++ update-i18n.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
+L=${1-pl}
 mkdir -p _build/locale
 make -e SPHINXOPTS="-E -a" gettext
-sphinx-intl update -d source/locale -p build/locale -l pl
+sphinx-intl update -d source/locale -p build/locale -l $L
